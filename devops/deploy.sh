ae76e74300c8c63484406cc482f9fd91eda96fe8
Madhavan46 <56082988+Madhavan46@users.noreply.github.com>
2024-05-30 02:20:31 +0530

    Update deploy.sh

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -7,12 +7,12 @@ tar -zcvf cropchat.tar.gz dist > /dev/null 2>&1
 echo 'Files compressed'
 
 echo 'Copying project to staging server...'
-scp cropchat.tar.gz ubuntu@cropchat.chbo.io:/home/ubuntu
-ssh ubuntu@cropchat.chbo.io rm -Rf /var/www/cropchat/current
-ssh ubuntu@cropchat.chbo.io mkdir /var/www/cropchat/current
+scp cropchat.tar.gz ubuntu@ip-172-31-20-32:/home/ubuntu
+ssh ubuntu@ip-172-31-20-32 rm -Rf /var/www/cropchat/current
+ssh ubuntu@ip-172-31-20-32 mkdir /var/www/cropchat/current
 
 echo 'Extracting files...'
-ssh ubuntu@cropchat.chbo.io tar -xvzf /home/ubuntu/cropchat.tar.gz -C /var/www/cropchat/current/ > /dev/null 2>&1
+ssh ubuntu@ip-172-31-20-32 tar -xvzf /home/ubuntu/cropchat.tar.gz -C /var/www/cropchat/current/ > /dev/null 2>&1
 echo 'Files extracted'
 
 echo 'Deployed successfully!'
